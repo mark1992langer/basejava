787d44d7274d0cb17dd360fb0c3e0c02c7b41857
Mark Gabrilovich <marklanger92@gmail.com>
2023-06-12 14:07:53 +0300

    Выполнение домашнего задания. Все методы были успешно реализованны.
    Метод clear я так понял, что он удаляет все резюме.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -4,27 +4,67 @@
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
+    int size;
+
     void clear() {
+        for (int i = 0; i < storage.length - 1; i++) {
+            if (storage[i] != null) {
+                storage[i] = null;
+            }
+        }
+        size();
     }
 
     void save(Resume r) {
+        for (int i = 0; i < storage.length - 1; i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < storage.length - 1; i++) {
+            if (storage[i] != null && storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < storage.length - 1; i++) {
+            if (storage[i] != null && storage[i].uuid.equals(uuid)) {
+                for (int j = i + 1; j < storage.length - 1; j++) {
+                    storage[j - 1] = storage[j];
+                }
+            }
+        }
+        size();
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
+
     Resume[] getAll() {
-        return new Resume[0];
+        size = size();
+        Resume[] allResume = new Resume[size];
+        for (int i = 0; i < storage.length - 1; i++) {
+            if (storage[i] != null) {
+                allResume[i] = storage[i];
+                size++;
+            }
+        }
+        return allResume;
     }
 
     int size() {
-        return 0;
+        size = 0;
+        for (int i = 0; i < storage.length - 1; i++) {
+            if (storage[i] != null) size++;
+        }
+        return size;
     }
 }
