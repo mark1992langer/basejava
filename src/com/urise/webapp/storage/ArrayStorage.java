1d395c84a7528de4b1f7d910681dd6463dbf69ca
Mark Gabrilovich <marklanger92@gmail.com>
2023-06-22 17:32:51 +0300

    Выполнение второй части проекта. Изменены методы класса ArrayStorage. Удален метод error.

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -9,80 +9,78 @@ import java.util.Arrays;
  */
 
 public class ArrayStorage {
-    private Resume[] storage = new Resume[10000];
+    private static final int STORAGE_LIMIT = 10000;
+    private final Resume[] storage = new Resume[STORAGE_LIMIT];
+
     private int size;
-    private String resumeString;
+    private int resumeIndex;
+    private Resume oldResume;
+    private  Resume updateResume = new Resume();
 
     public void clear() {
-        Arrays.fill(storage,0, size-1, null);
+        Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
     public void update(Resume r) {
-        resumeString = r.getUuid();
-        if (isResume(resumeString)) {
-            r.setUuid("uuid10");
-        } else error(0, resumeString);
+        resumeIndex = getIndex(r.getUuid());
+        if (resumeIndex != -1) {
+            oldResume = r;
+            updateResume.setUuid("uuid10");
+            storage[resumeIndex] = updateResume;
+        } else {
+            System.out.println("resume:" + r.getUuid() + " not found!");
+        }
     }
 
     public void save(Resume r) {
-        resumeString = r.getUuid();
-        if (!isResume(resumeString)) {
-            if (size < storage.length) {
-                storage[size] = r;
-                size++;
-            } else error(2, resumeString);
-        } else error(1, resumeString);
-
+        resumeIndex = getIndex(r.getUuid());
+        if(size >= storage.length){
+            System.out.println("the base is full!");
+        }else if (resumeIndex != -1){
+            System.out.println("resume:" + r.getUuid() + " has already!");
+        }else {
+            storage[size] = r;
+            size++;
+        }
     }
 
     public Resume get(String uuid) {
-        if (isResume(uuid)) {
-            for (int i = 0; i < size; i++) {
-                if (storage[i].getUuid().equals(uuid)) {
-                    return storage[i];
-                }
-            }
-        } else error(0, uuid);
+        resumeIndex = getIndex(uuid);
+        if (resumeIndex != -1) {
+            return storage[resumeIndex];
+        } else {
+            System.out.println("resume:" + uuid + " not found!");
+        }
         return null;
     }
 
     public void delete(String uuid) {
-        if (isResume(uuid)) {
-            for (int i = 0; i < size; i++) {
-                if (storage[i].getUuid().equals(uuid)) {
-                    storage[i] = storage[size - 1];
-                    storage[size - 1] = null;
-                    size--;
-                    break;
-                }
-            }
-        } else error(0, uuid);
+        resumeIndex = getIndex(uuid);
+        if (resumeIndex != -1) {
+            storage[resumeIndex] = storage[size - 1];
+            storage[size - 1] = null;
+            size--;
+        } else {
+            System.out.println("resume:" + uuid + " not found!");
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     public Resume[] getAll() {
-        return Arrays.copyOfRange(storage,0 ,size);
+        return Arrays.copyOfRange(storage, 0, size);
     }
 
     public int size() {
         return size;
     }
 
-    public boolean isResume(String uuid) {
+    public int getIndex(String uuid) {
         for (int i = 0; i < size; i++) {
-            if (storage[i].getUuid().equals(uuid)) return true;
+            if (storage[i].getUuid().equals(uuid)) return i;
         }
-        return false;
-    }
-
-    public void error(int a, String b) {
-        switch (a) {
-            case 0 -> System.out.println("resume:" + b + " not found!");
-            case 1 -> System.out.println("resume:" + b + " has already!");
-            case 2 -> System.out.println("the base is full!");
-        }
+        return -1;
     }
 }
