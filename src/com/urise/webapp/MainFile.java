cba68c115cd6e4154be3cdccc5916adce2cfa112
Mark Gabrilovich <marklanger92@gmail.com>
2023-08-01 19:29:54 +0300

    hw9 adding margins to class MainFile

--- src/com/urise/webapp/MainFile.java
+++ src/com/urise/webapp/MainFile.java
@@ -5,24 +5,30 @@ import java.io.File;
 public class MainFile {
     public static void main(String[] args) {
         File dir = new File(System.getProperty("user.dir"));
-        dirRecursion(dir);
+        File dirSrc = new File("src");
+        dirRecursion(dirSrc, 0);
     }
 
-    public static void dirRecursion(File dir) {
+    public static void dirRecursion(File dir, int level) {
+        final String indent = indented(level, "     ");
         if (dir.isDirectory()) {
             File[] files = dir.listFiles();
             if (files != null) {
                 for (File file : files) {
                     if (file.isFile()) {
                         if (file.getName().endsWith(".java")) {
-                            System.out.println(file.getName());
+                            System.out.println(indent + "File: " + file.getName());
                         }
-                    }
-                    if (file.isDirectory()) {
-                        dirRecursion(file);
+                    }else if(file.isDirectory()) {
+                        System.out.println(indent + "Directory: " + file.getName());
+                        dirRecursion(file, level + 1);
                     }
                 }
             }
         }
+    }
+
+    public static String indented(int i, String value) {
+        return new String(new char[i]).replace("\0", value);
     }
 }
