555d42152f65e12fe7d6a76b0e9a7358590b291b
Mark Gabrilovich <marklanger92@gmail.com>
2023-08-07 19:36:29 +0300

    hw10 JSON

--- test/com/urise/webapp/storage/AllStorageTest.java
+++ test/com/urise/webapp/storage/AllStorageTest.java
@@ -12,6 +12,7 @@ import org.junit.runners.Suite;
         ObjectFileStorageTest.class,
         ObjectPathStorageTest.class,
         XmlPathStorageTest.class,
+        JsonPathStorageTest.class,
 })
 public class AllStorageTest {
 }
