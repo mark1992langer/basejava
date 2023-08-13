6669d8d086475bb9442f5199dff8cb43aef9d969
Mark Gabrilovich <marklanger92@gmail.com>
2023-08-13 19:35:08 +0300

    hw11 Deadlock

--- src/com/urise/webapp/DeadlockExample.java
+++ src/com/urise/webapp/DeadlockExample.java
@@ -6,41 +6,26 @@ import java.util.concurrent.locks.ReentrantLock;
 import static java.lang.Thread.sleep;
 
 public class DeadlockExample {
-    private final Lock rightLock = new ReentrantLock();
-    private final Lock leftLock = new ReentrantLock();
+    private static final Lock rightLock = new ReentrantLock();
+    private static final Lock leftLock = new ReentrantLock();
 
     public static void main(String[] args) {
         DeadlockExample de = new DeadlockExample();
-        new Thread(de::operationRight, "rightThread").start();
-        new Thread(de::operationLeft, "leftThread").start();
+        new Thread(() -> de.operation(leftLock, rightLock)).start();
+        new Thread(() -> de.operation(rightLock, leftLock)).start();
     }
 
-    public void operationRight(){
-        rightLock.lock();
-        System.out.println("rightLock acquired, waiting to acquire leftLock");
+    private void operation(Lock lock1, Lock lock2) {
+        lock1.lock();
+        System.out.println("Lock acquired, waiting to acquire second lock");
         try {
             sleep(50);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        leftLock.lock();
-        System.out.println("leftLock acquired");
-        leftLock.unlock();
-        rightLock.unlock();
-
-    }
-
-    public void operationLeft(){
-        leftLock.lock();
-        System.out.println("leftLock acquired, waiting to acquire rightLock");
-        try {
-            sleep(50);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        rightLock.lock();
-        System.out.println("rightLock acquired");
-        rightLock.unlock();
-        leftLock.unlock();
+        lock2.lock();
+        System.out.println("acquired");
+        lock2.unlock();
+        lock1.unlock();
     }
 }
