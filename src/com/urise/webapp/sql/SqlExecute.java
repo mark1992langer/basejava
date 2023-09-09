bb47d152ad3151853f1aa3192485cf4ba9048c18
Mark Gabrilovich <marklanger92@gmail.com>
2023-09-09 18:50:25 +0300

    hw15 add lesson HTML, Tomcat

--- src/com/urise/webapp/sql/SqlExecute.java
+++ src/com/urise/webapp/sql/SqlExecute.java
@@ -2,7 +2,7 @@ package com.urise.webapp.sql;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-
+@FunctionalInterface
 public interface SqlExecute <T> {
     T execute(PreparedStatement ps) throws SQLException;
 }
