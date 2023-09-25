c095fe5ba215c7b9c63158ca4087ccfd8168ae0d
Mark Gabrilovich <marklanger92@gmail.com>
2023-09-25 18:42:40 +0300

    hw16 add view and edit

--- src/com/urise/webapp/web/ResumeServlet.java
+++ src/com/urise/webapp/web/ResumeServlet.java
@@ -1,6 +1,8 @@
 package com.urise.webapp.web;
 
 import com.urise.webapp.Config;
+import com.urise.webapp.model.ContactType;
+import com.urise.webapp.model.Resume;
 import com.urise.webapp.storage.Storage;
 
 import javax.servlet.ServletConfig;
@@ -21,55 +23,49 @@ public class ResumeServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setAttribute("resumes", storage.getAllSorted());
-        request.getRequestDispatcher("/WEB-INF/jsp/list.jsp").forward(request, response);
-//        request.setCharacterEncoding("UTF-8");
-//        response.setCharacterEncoding("UTF-8");
-//        response.setContentType("text/html; charset=UTF-8");
-//        Writer writer = response.getWriter();
-//        writer.write(
-//                """
-//                        <html>
-//                        <head>
-//                            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-//                            <title>Список всех резюме</title>
-//                            <style>
-//                               .center {
-//                                margin: auto;
-//                                border: 3px solid #006400;
-//                              }
-//                              </style>
-//                        </head>
-//                        <body>
-//                        <section>
-//                        <table class="center" border="2" cellpadding="8" cellspacing="2">
-//                            <tr>
-//                                <th>NAME</th>
-//                                <th>TELEPHONE</th>
-//                                <th>SKYPE</th>
-//                                <th>Email</th>
-//                            </tr>
-//                        """);
-//        for (Resume resume : storage.getAllSorted()) {
-//            writer.write(
-//                    "<tr>\n" +
-//                    "     <td>" + resume.getFullName() + "</td>\n" +
-//                    "     <td>" + resume.getContact(ContactType.TELEPHONE) + "</td>\n" +
-//                    "     <td>" + resume.getContact(ContactType.SKYPE) + "</td>\n" +
-//                    "     <td>" + resume.getContact(ContactType.MAIL) + "</td>\n" +
-//                    "</tr>\n");
-//        }
-//        writer.write("""
-//                </table>
-//                </section>
-//                </body>
-//                </html>
-//                """);
+        String uuid = request.getParameter("uuid");
+        String action = request.getParameter("action");
+        if (action == null) {
+            request.setAttribute("resumes", storage.getAllSorted());
+            request.getRequestDispatcher("/WEB-INF/jsp/list.jsp").forward(request, response);
+            return;
+        }
+        Resume r;
+        switch (action) {
+            case "delete":
+                storage.delete(uuid);
+                response.sendRedirect("resume");
+                return;
+            case "view":
+            case "edit":
+                r = storage.get(uuid);
+                break;
+            default:
+                throw new IllegalArgumentException("Action " + action + " is illegal");
+        }
+        request.setAttribute("resume", r);
+        request.getRequestDispatcher(
+                ("view".equals(action) ? "/WEB-INF/jsp/view.jsp" : "/WEB-INF/jsp/edit.jsp")
+        ).forward(request, response);
     }
 
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        request.setCharacterEncoding("UTF-8");
+        String uuid = request.getParameter("uuid");
+        String fullName = request.getParameter("fullName");
+        Resume r = storage.get(uuid);
+        r.setFullName(fullName);
+        for (ContactType type : ContactType.values()) {
+            String value = request.getParameter(type.name());
+            if (value != null && !value.trim().isEmpty()) {
+                r.setContact(type, value);
+            } else {
+                r.getContacts().remove(type);
+            }
+        }
+        storage.update(r);
+        response.sendRedirect("resume");
     }
 }
